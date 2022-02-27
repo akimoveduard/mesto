fe14cef22e0c018cdcf9d74a7e89a8475aecd636
Eduard <akimov-eduard@yandex.ru>
2022-02-27 13:31:22 +0300

    feat: добавить функционал попапа для места

--- js/index.js
+++ js/index.js
@@ -28,9 +28,12 @@ const initialCards = [
   }
 ];
 
-// template
+// templates
 const photoTemplate = document.querySelector('#photo-grid-item-template').content;
 const photoGrid = document.querySelector('.photo-grid__list');
+
+const mestoTemplate = document.querySelector('#mesto-template').content;
+const mestoWrapper = document.querySelector('.mesto');
 
 // Профиль
 const buttonProfileOpen = document.querySelector('[name="profile-button-open"]');
@@ -47,6 +50,78 @@ const buttonAddnewClose = document.querySelector('[name="addnew-button-close"]')
 
 const inputAddnewCaption = document.querySelector('[name="addnew-input-caption"]');
 const inputAddnewImage = document.querySelector('[name="addnew-input-image"]');
+
+// Mesto
+const buttonMestoClose = document.querySelector('[name="button-close-mesto"]');
+
+/* ФУНКЦИИ */
+
+// Установки разных слушателей
+function setEventListeners(itemElement) {
+  itemElement.querySelector('.button_type_delete').addEventListener('click', removePlace);
+  itemElement.querySelector('.button_type_like').addEventListener('click', likePlace);
+  itemElement.querySelector('.photo-grid__item-image').addEventListener('click', showMesto);
+}
+
+// Открытие всплывающего окна
+function openPopup(element) {
+  objectPopup = document.querySelector(element);
+  if (!objectPopup.classList.contains('popup_opened')) {
+    objectPopup.classList.add('popup_opened');
+    document.addEventListener('keydown', (evt) => {
+      if (evt.key === 'Escape') {
+        objectPopup.classList.remove('popup_opened');
+        if (document.querySelector('.mesto__figure')) {
+          document.querySelector('.mesto__figure').remove();
+        }
+      }
+    },
+    true);
+  }
+}
+
+// Закрытие всплывающего окна
+function closePopup(element) {
+  objectPopup = document.querySelector(element);
+  if (objectPopup.classList.contains('popup_opened')) {
+    objectPopup.classList.remove('popup_opened');
+  }
+}
+
+// Создание новой карточки
+function renderPlace(element) {
+  const itemElement = photoTemplate.querySelector('.photo-grid__item').cloneNode(true);
+  itemElement.querySelector('.photo-grid__item-image').src = element.link;
+  itemElement.querySelector('.photo-grid__item-caption').textContent = element.name;
+  setEventListeners(itemElement);
+  photoGrid.prepend(itemElement);
+}
+
+// Удаление карточки
+function removePlace(event) {
+  const itemElement = event.target.closest(".photo-grid__item");
+  itemElement.remove();
+}
+
+// Лайк карточки
+function likePlace(event) {
+  const itemElement = event.target;
+  itemElement.classList.toggle('button_type_liked');
+}
+
+// Mesto
+function showMesto(event) {
+  const itemElement = mestoTemplate.querySelector('.mesto__figure').cloneNode(true);
+  itemElement.querySelector('.mesto__image').src = event.target.src;
+  itemElement.querySelector('.mesto__caption').textContent = event.target.nextElementSibling.querySelector('.photo-grid__item-caption').textContent;
+  mestoWrapper.prepend(itemElement);
+  openPopup('.popup_type_mesto');
+}
+
+function closeMesto() {
+  document.querySelector('.mesto__figure').remove();
+  closePopup('.popup_type_mesto');
+}
 
 /* СЛУШАТЕЛИ */
 
@@ -87,55 +162,8 @@ formAddnew.addEventListener('submit', (event)=> {
   closePopup('.popup_type_addnew');
 });
 
-function setEventListeners(itemElement) {
-  itemElement.querySelector('.button_type_delete').addEventListener('click', removePlace);
-  itemElement.querySelector('.button_type_like').addEventListener('click', likePlace);
-}
-
-/* ФУНКЦИИ */
-
-// Открытие всплывающего окна
-function openPopup(element) {
-  objectPopup = document.querySelector(element);
-  if (!objectPopup.classList.contains('popup_opened')) {
-    objectPopup.classList.add('popup_opened');
-    document.addEventListener('keydown', (evt) => {
-      if (evt.key === 'Escape') {
-        objectPopup.classList.remove('popup_opened');
-      }
-    },
-    true);
-  }
-}
-
-// Закрытие всплывающего окна
-function closePopup(element) {
-  objectPopup = document.querySelector(element);
-  if (objectPopup.classList.contains('popup_opened')) {
-    objectPopup.classList.remove('popup_opened');
-  }
-}
-
-// Создание новой карточки
-function renderPlace(element) {
-  const itemElement = photoTemplate.querySelector('.photo-grid__item').cloneNode(true);
-  itemElement.querySelector('.photo-grid__item-image').src = element.link;
-  itemElement.querySelector('.photo-grid__item-caption').textContent = element.name;
-  setEventListeners(itemElement);
-  photoGrid.append(itemElement);
-}
-
-// Удаление карточки
-function removePlace(event) {
-  const itemElement = event.target.closest(".photo-grid__item");
-  itemElement.remove();
-}
-
-// Лайк карточки
-function likePlace(event) {
-  const itemElement = event.target;
-  itemElement.classList.toggle('button_type_liked');
-}
+// Mesto
+buttonMestoClose.addEventListener('click', closeMesto);
 
 /* ЛОГИКА */
 initialCards.forEach(renderPlace);
