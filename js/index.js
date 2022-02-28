aa78860b74d6cc16f8a3333749fad6a61ceae95e
Eduard <akimov-eduard@yandex.ru>
2022-02-28 22:08:58 +0300

    hotfix: исправить замечания после код-ревью

--- js/index.js
+++ js/index.js
@@ -1,44 +1,11 @@
 /* ПЕРЕМЕННЫЕ */
 
-// Карточки мест по умолчанию
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
-// templates
-const photoTemplate = document.querySelector('#photo-grid-item-template').content;
-const photoGrid = document.querySelector('.photo-grid__list');
-
-const mestoTemplate = document.querySelector('#mesto-template').content;
-const mestoWrapper = document.querySelector('.mesto');
-
 // Профиль
 const buttonProfileOpen = document.querySelector('[name="profile-button-open"]');
 const buttonProfileClose = document.querySelector('[name="button-close-profile"]');
 
+const elementProfileName = document.querySelector('.profile__name');
+const elementProfileAbout = document.querySelector('.profile__about');
 const formProfile = document.querySelector('[name="profile-form"]');
 const inputProfileName = document.querySelector('[name="profile-input-name"]');
 const inputProfileAbout = document.querySelector('[name="profile-input-about"]');
@@ -51,51 +18,60 @@ const buttonAddnewClose = document.querySelector('[name="addnew-button-close"]')
 const inputAddnewCaption = document.querySelector('[name="addnew-input-caption"]');
 const inputAddnewImage = document.querySelector('[name="addnew-input-image"]');
 
-// Mesto
+// Место
+const mestoGrid = document.querySelector('.photo-grid__list');
+
+// Модальное окно места
+const mestoImage = document.querySelector('.mesto__image');
+const mestoCaption = document.querySelector('.mesto__caption');
+
 const buttonMestoClose = document.querySelector('[name="button-close-mesto"]');
 
 /* ФУНКЦИИ */
 
-// Установки разных слушателей
-function setEventListeners(itemElement) {
+// Установки разных слушателей на карточку места
+function setCardEventListeners(itemElement) {
   itemElement.querySelector('.button_type_delete').addEventListener('click', removeMesto);
   itemElement.querySelector('.button_type_like').addEventListener('click', likeMesto);
-  itemElement.querySelector('.photo-grid__item-image').addEventListener('click', showMesto);
+  itemElement.querySelector('.photo-grid__item-image').addEventListener('click', () => {
+    createPopupMesto(itemElement);
+    openPopup('.popup_type_mesto');
+  });
 }
 
 // Открытие всплывающего окна
 function openPopup(element) {
-  objectPopup = document.querySelector(element);
-  if (!objectPopup.classList.contains('popup_opened')) {
-    objectPopup.classList.add('popup_opened');
-    document.addEventListener('keydown', (evt) => {
-      if (evt.key === 'Escape') {
-        objectPopup.classList.remove('popup_opened');
-        if (document.querySelector('.mesto__figure')) {
-          document.querySelector('.mesto__figure').remove();
-        }
-      }
-    },
-    true);
-  }
+  document.querySelector(element).classList.add('popup_opened');
 }
 
 // Закрытие всплывающего окна
 function closePopup(element) {
-  objectPopup = document.querySelector(element);
-  if (objectPopup.classList.contains('popup_opened')) {
-    objectPopup.classList.remove('popup_opened');
-  }
+  document.querySelector(element).classList.remove('popup_opened');
 }
 
-// Создание новой карточки
-function renderMesto(element) {
-  const itemElement = photoTemplate.querySelector('.photo-grid__item').cloneNode(true);
-  itemElement.querySelector('.photo-grid__item-image').src = element.link;
-  itemElement.querySelector('.photo-grid__item-image').alt = element.name;
-  itemElement.querySelector('.photo-grid__item-caption').textContent = element.name;
-  setEventListeners(itemElement);
-  photoGrid.prepend(itemElement);
+// Получение значений из HTML-элементов для формы профиля
+function getProfileValues() {
+  inputProfileName.value = elementProfileName.textContent;
+  inputProfileAbout.value = elementProfileAbout.textContent;
+}
+
+// Установка новых значений из формы профиля в HTML-элементы
+function setProfileValues () {
+  if (inputProfileName) { elementProfileName.textContent = inputProfileName.value; }
+  if (inputProfileAbout) { elementProfileAbout.textContent = inputProfileAbout.value; }
+}
+
+// Создание карточки
+function createCard(elementLink, elementName) {
+  const cardTemplate = document.querySelector('#photo-grid-item-template').content;
+  const newCard = cardTemplate.querySelector('.photo-grid__item').cloneNode(true);
+  const cardImage = newCard.querySelector('.photo-grid__item-image');
+  const cardCaption = newCard.querySelector('.photo-grid__item-caption');
+  cardImage.src = elementLink;
+  cardImage.alt = elementName;
+  cardCaption.textContent = elementName;
+  setCardEventListeners(newCard);
+  return newCard;
 }
 
 // Удаление карточки
@@ -110,61 +86,65 @@ function likeMesto(event) {
   itemElement.classList.toggle('button_type_liked');
 }
 
-// Mesto
-function showMesto(event) {
-  const itemElement = mestoTemplate.querySelector('.mesto__figure').cloneNode(true);
-  itemElement.querySelector('.mesto__image').src = event.target.src;
-  itemElement.querySelector('.mesto__caption').textContent = event.target.nextElementSibling.querySelector('.photo-grid__item-caption').textContent;
-  mestoWrapper.prepend(itemElement);
-  openPopup('.popup_type_mesto');
-}
-
-function closeMesto() {
-  document.querySelector('.mesto__figure').remove();
-  closePopup('.popup_type_mesto');
+// Подготовка модального окна с местом
+function createPopupMesto(itemElement) {
+  itemImage = itemElement.querySelector('.photo-grid__item-image');
+  mestoImage.src = itemImage.src;
+  mestoImage.alt = itemImage.alt;
+  mestoCaption.textContent = itemElement.querySelector('.photo-grid__item-caption').textContent;
 }
 
 /* СЛУШАТЕЛИ */
 
-// Профиль
+// Открытие модального окна профиля
 buttonProfileOpen.addEventListener('click', () => {
+  getProfileValues();
   openPopup('.popup_type_profile');
-  inputProfileName.value = document.querySelector('.profile__name').textContent;
-  inputProfileAbout.value = document.querySelector('.profile__about').textContent;
 });
 
+
+// Закрытие модального окна профиля
 buttonProfileClose.addEventListener('click', () => {
   closePopup('.popup_type_profile');
 });
 
+// Обработка ввода данных формы профиля
 formProfile.addEventListener('submit', (event) => {
   event.preventDefault();
-  if (inputProfileName) { document.querySelector('.profile__name').textContent = inputProfileName.value; }
-  if (inputProfileAbout) { document.querySelector('.profile__about').textContent = inputProfileAbout.value; }
+  setProfileValues();
   closePopup('.popup_type_profile');
 });
 
-// Добавление места
+// Открытие модального окна формы нового места
 buttonAddnewOpen.addEventListener('click', () => {
   openPopup('.popup_type_addnew');
 });
 
+// Закрытие модального окна нового места
 buttonAddnewClose.addEventListener('click', () => {
   closePopup('.popup_type_addnew');
 });
 
+// Обработка ввода данных формы места
 formAddnew.addEventListener('submit', (event)=> {
   event.preventDefault();
-  const newPlace = {
+  const newMesto = {
     name: inputAddnewCaption.value,
     link: inputAddnewImage.value
   };
-  renderPlace(newPlace);
+  const newCard = createCard(newMesto.link, newMesto.name);
+  mestoGrid.prepend(newCard);
   closePopup('.popup_type_addnew');
 });
 
-// Mesto
-buttonMestoClose.addEventListener('click', closeMesto);
+// Закрытие модального окна места
+buttonMestoClose.addEventListener('click', () => {
+  closePopup('.popup_type_mesto');
+});
 
 /* ЛОГИКА */
-initialCards.forEach(renderMesto);
+
+initialCards.forEach(function (item) {
+  newCard = createCard(item.link, item.name);
+  mestoGrid.append(newCard);
+});
